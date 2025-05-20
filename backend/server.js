6fb24a035f8a209393f1ccf11fb71eb2ee51373d
hritik1234567 <ritikraheja82@gmail.com>
2025-05-20 17:45:36 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import { app, server } from "./socket/socket.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const PORT = process.env.PORT || 8000;
 
-const app=express();
+
 dotenv.config();
+const __dirname = path.resolve();
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-app.get("/",(req,res)=>{
-    res.send("Hello!");
-});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
